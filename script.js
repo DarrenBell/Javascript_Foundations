5bf616b1565329e5d623e67657a3756deec3ea25
Darren Bell <darren.l.bell@gmail.com>
2015-03-16 18:58:30 +1100

    variables
    information all about variables and explanations, rules and best
    practices

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
-document.getElementById("btn").addEventListener("click", function  () {
-	console.log("Hey this works too!");
-});
+var message;
+// created a variable
+message = "Hello, world.";
+// assignment operator is the "=" sign
+// "Hello, world." is the data stored
+// surround text with single or double quotes, this is a string
+
+console.log(message);
+// log the variable to the console / passed a variable
+
+message = "Everything changes."
+// variables can change
+
+console.log(message);
+message = 101;
+// numbers are the other data type
+
+console.log(message);
+// any variable can hold any data type
+
+message = true;
+// true and false are boolean, quotations not required
+console.log(message);
+
+var firstName = "John",
+		lastName = "Smith",
+		age;
+// declare and assign a variables to save a line of code
+// only need to use var once to declare multiple variables
+// you can assign data to variables when declaring
+
+age = 30;
+console.log(firstName, lastName, age)
+// you can log multiple variables separated by commas
+// variables must be named using either an uppercase, lowercase, underscore or dollar sign
+// other preceding characters can be anything else
+// variables cannot have keywords that are reserved - function, return, var, super for example
+// variabled are ususally named in camelcase - camelCase
+// use variable names that mean something, do not abbreviate
+// typing variable name in the console shows the data
+// typing alert(variable) will show a popup
