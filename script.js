1c970ac8ceeaed90394d845288a9bcfa5a6003e0
Darren Bell <darren.l.bell@gmail.com>
2015-03-18 20:36:48 +1100

    numbers
    all about numbers in Javascript, including modulus operator, add and
    assign operator, prefix operator, postfix operator and order of sums in
    math.  Also math.random stuff.

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
-var message;
-// created a variable
-message = "Hello, world.";
-// assignment operator is the "=" sign
-// "Hello, world." is the data stored
-// surround text with single or double quotes, this is a string
+5 + 5;
+// addition
+10 - 7;
+// subtraction
+3 * 4;
+// multiplication
+100 / 20;
+// division
+13 % 5;
+// % is the modulus operator, it returns the remainder of a division (answer = 3)
+var total = 10;
 
-console.log(message);
-// log the variable to the console / passed a variable
+total = total + 5;
+// store result back in variable
+total += 5;
+// add and assign operator - adds then assigns new value
+var count 0
+// variable starts at 0 in this instance
+count +=1;
+// count +=1; would increase the variable count by 1
+++count;
+// the increment operator adds 1 to variable
+// ++ before variable is a prefix operator, when this is used it adds 1 to the variable then it can be used 
+// ++ after variable is a postfix operator, when this is used the original variable will be used then increment after
+// if ++ used after the variable that variable can be used in next 
 
-message = "Everything changes."
-// variables can change
+1+1 *2;
+// order of operation - multiplication first
 
-console.log(message);
-message = 101;
-// numbers are the other data type
+(1 + 1) *2;
+// order of operation - sum in brackets first then multiplication
 
-console.log(message);
-// any variable can hold any data type
-
-message = true;
-// true and false are boolean, quotations not required
-console.log(message);
-
-var firstName = "John",
-		lastName = "Smith",
-		age;
-// declare and assign a variables to save a line of code
-// only need to use var once to declare multiple variables
-// you can assign data to variables when declaring
-
-age = 30;
-console.log(firstName, lastName, age)
-// you can log multiple variables separated by commas
-// variables must be named using either an uppercase, lowercase, underscore or dollar sign
-// other preceding characters can be anything else
-// variables cannot have keywords that are reserved - function, return, var, super for example
-// variabled are ususally named in camelcase - camelCase
-// use variable names that mean something, do not abbreviate
-// typing variable name in the console shows the data
-// typing alert(variable) will show a popup
+Math.max(5, 10);
+// math.max takes two or more numbers then returns the larger number
+Math.sqrt(49);
+// math.sqrt works out the square root of the number
+Math.round(3.6);
+// math.round rounds to the nearest whole number
+Math.random()
+// gives a random number between 0 and 1
+Math.random() * 100
+// multiplying math.random by a number gives a higher random number return
